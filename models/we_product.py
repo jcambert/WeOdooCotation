8d38a34cb1703e06a2440f99a7ea5e54c1a77e1d
Ambert Jean-Christophe <jc.ambert@free.fr>
2021-02-14 21:01:33 +0100

    updates

--- models/we_product.py
+++ models/we_product.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+
+class WeIndice(models.Model):
+    _inherit=['']
 class WeProduct(models.Model):
     _inherit = ['product.template']
     cotations=fields.One2many('we.cotation','product')
-    state=fields.Selection([
-        ('normal',)
-    ],)
+    state=fields.Selection(selection_add=[('cotation', 'Cotation')],default='')
